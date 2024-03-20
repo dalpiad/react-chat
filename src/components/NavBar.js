ddda5818bfe817799b5c930f9d93d3b1338f54f0
dalpiad <dalpiad@gmail.com>
2024-03-20 12:45:09 -0500

    updated SendMessage and Chatbox

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
 
 const NavBar = () => {
-  const [user, setUser] = useState(false);
+  const [user] = useAuthState(auth);;
 
   const googleSignIn = () => {
-    setUser(true);
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
   };
-
   const signOut = () => {
-    setUser(false);
+    auth.signOut();
   };
 
   return (
